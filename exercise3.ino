bb04b7a8e32db10adc8c57a83d277f28d16b004a
Tarho <115691708+Tarho@users.noreply.github.com>
2024-04-02 10:58:21 +0700

    Update exercise3.ino

--- exercise3.ino
+++ exercise3.ino
@@ -1,20 +1,21 @@
-int PIR_PIN = 2;
-int motionState = LOW;
+int PINR = 2;
+int MState = LOW;
 
 void setup() {
-  pinMode(PIR_PIN, INPUT);
+  pinMode(PINR, INPUT);
   Serial.begin(9600);
     Serial.println("Waiting for PIR to stabilize");
-  delay(1000); // Wait for 1 second
+  delay(1000); 
     Serial.println("Ready to detect motion!");
 }
  
 void loop() {
-  motionState = digitalRead(PIR_PIN);
-    if (motionState == HIGH) {
+  MState = digitalRead(PINR);
+    if (MState == HIGH) {
     Serial.println("Motion detected!");
-    delay(500); 
+    delay(50); 
   } else {
     Serial.println("No motion detected.");
-    delay(500);   }
+    delay(50);   }
 }
+
